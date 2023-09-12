0dd82ae3d3d4370da21376d1bd8192640827b0b2
Manfred Rosenboom <maroph@gmail.com>
2023-09-12 17:14:48 +0200

    use a JavaScript for the page footers

---
+++ assets/js/footer.js
@@ -0,0 +1,40 @@
+count = 0;
+for (const char of window.location.pathname) {
+    if (char === '/') {
+        count++;
+    }
+}
+if (window.location.pathname.startsWith('/projects/')) {
+    count -= 3;
+} else {
+    count -= 1;
+}
+if (count < 0) {
+    count = 0;
+}
+let dirprefix = '.';
+if (count > 0) {
+    dirprefix = '..';
+    for (let i = 1; i < count; i++) {
+        dirprefix = dirprefix + '/..';
+    }
+}
+//
+/*
+document.writeln('<pre>');
+document.writeln('window.location.pathname : ' + window.location.pathname);
+document.writeln('dirprefix                : ' + dirprefix);
+document.writeln('</pre>');
+*/
+//
+document.writeln('<footer>');
+document.writeln('<hr>');
+document.write('<a href="https://validator.w3.org/nu/?doc=');
+document.write(window.location);
+document.writeln('" target="_blank"><img src="' + dirprefix + '/assets/images/valid-html5-blue.png" alt="valid HTML5 code" title="valid HTML5 code"></a>&nbsp;&nbsp;');
+document.write('<a href="https://jigsaw.w3.org/css-validator/validator?uri=');
+document.write(window.location);
+document.writeln('" target="_blank"><img src="' + dirprefix + '/assets/images/valid-css-blue.png" alt="valid CSS code" title="valid CSS code"></a><br>');
+document.writeln('<a rel="license,nofollow" href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank"><img src="' + dirprefix + '/assets/images/cc-by-sa-88x31.png" alt="CC-BY-SA 4.0" title="CC-BY-SA 4.0"></a>&nbsp;&nbsp;');
+document.write('(c) 2023, <a href="https://maroph.github.io/" target="_blank" rel="noreferrer">Manfred Rosenboom</a> (<a href="mailto:maroph@pm.me">maroph@pm.me</a>)');
+document.writeln('</footer>');
